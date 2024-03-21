cfd8a26852632fa6f44878464cfac8a88ec75fae
Stanislau <it.senkevich@gmail.com>
2024-03-21 12:19:39 +0300

    init include/config.h

--- include/config.h
+++ include/config.h
@@ -5,4 +5,31 @@
 #ifndef LAB_5_PROJECT_CONFIG_H
 #define LAB_5_PROJECT_CONFIG_H
 
+#include <string>
+
+class Config {
+private:
+    double driverPercent;
+    std::string hashSalt;
+
+public:
+    double getDriverPercent() const {
+        return driverPercent;
+    }
+
+    std::string getHashSalt() const {
+        return hashSalt;
+    }
+
+    void setDriverPercent(double newDriverPercent) {
+        driverPercent = newDriverPercent;
+    }
+
+    void setHashSalt(const std::string& newHashSalt) {
+        hashSalt = newHashSalt;
+    }
+};
+
+
+
 #endif //LAB_5_PROJECT_CONFIG_H
