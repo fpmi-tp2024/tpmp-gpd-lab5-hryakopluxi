981857e280b2a502879c4b4b4956a4d98642347a
Stanislau <it.senkevich@gmail.com>
2024-03-21 15:11:58 +0300

    init include/view.h

--- include/view.h
+++ include/view.h
@@ -5,4 +5,48 @@
 #ifndef LAB_5_PROJECT_VIEW_H
 #define LAB_5_PROJECT_VIEW_H
 
+#include "controller.h"
+
+class View {
+private:
+    std::string db_filename = "db/app.db";
+    Controller controller;
+
+public:
+    View();
+
+    void addDb();
+    void initController();
+
+    void login();
+    void logout();
+
+    void menu() const;
+
+    void getDriverOrders() const;
+    void getCarMileageAndLoads() const;
+    void getDriverSummaryWork() const;
+    void getWorstDriverSummary() const;
+    void getBestCarSummary() const;
+
+    void storeDriversEarnedMoney();
+    void getDriverEarnedMoney();
+
+    void addCar();
+    void addDriver();
+    void addOrder();
+    void addDispatcher();
+
+    void updateCar();
+    void updateDriver();
+    void updateOrder();
+    void updateDispatcher();
+
+    void deleteCar();
+    void deleteDriver();
+    void deleteOrder();
+    void deleteDispatcher();
+
+};
+
 #endif //LAB_5_PROJECT_VIEW_H
