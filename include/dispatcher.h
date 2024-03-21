6320ed6b6e28488cbf9e86135fda18de64a69247
Stanislau <it.senkevich@gmail.com>
2024-03-21 12:00:24 +0300

    init include/disaptcher.h

--- include/dispatcher.h
+++ include/dispatcher.h
@@ -2,7 +2,51 @@
 // Created by hakeyn on 21.3.24.
 //
 
-#ifndef LAB_5_PROJECT_DISPATCHR_H
-#define LAB_5_PROJECT_DISPATCHR_H
+#ifndef LAB_5_PROJECT_DISPATCHER_H
+#define LAB_5_PROJECT_DISPATCHER_H
 
-#endif //LAB_5_PROJECT_DISPATCHR_H
+#include "user.h"
+
+class Dispatcher : public User {
+private:
+    std::string name;
+    std::string surname;
+    std::string address;
+    std::string city;
+
+public:
+    std::string getName() const {
+        return name;
+    }
+
+    std::string getSurname() const {
+        return surname;
+    }
+
+    std::string getAddress() const {
+        return address;
+    }
+
+    std::string getCity() const {
+        return city;
+    }
+
+    void setName(const std::string& newName) {
+        name = newName;
+    }
+
+    void setSurname(const std::string& newSurname) {
+        surname = newSurname;
+    }
+
+    void setAddress(const std::string& newAddress) {
+        address = newAddress;
+    }
+
+    void setCity(const std::string& newCity) {
+        city = newCity;
+    }
+
+};
+
+#endif //LAB_5_PROJECT_DISPATCHER_H
