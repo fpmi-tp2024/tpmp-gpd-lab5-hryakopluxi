bd9c24c6ba6359163622821d6b0fc350b5abc306
Stanislau <it.senkevich@gmail.com>
2024-03-21 12:23:57 +0300

    init include/validator.h

--- include/validator.h
+++ include/validator.h
@@ -5,4 +5,18 @@
 #ifndef LAB_5_PROJECT_VALIDATOR_H
 #define LAB_5_PROJECT_VALIDATOR_H
 
+#include "config.h"
+#include "order.h"
+#include "driver.h"
+
+class Validator {
+    const Config* config;
+
+public:
+    bool validOrder(const Order& order) const;
+    bool validDriver(const Driver& driver) const;
+    bool validLicense(std::string license) const;
+    bool validPassword(std::string password, std::string salt) const;
+};
+
 #endif //LAB_5_PROJECT_VALIDATOR_H
