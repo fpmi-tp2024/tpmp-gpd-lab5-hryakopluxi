734626430cfc81afdb0da180b04a5e810d336ab4
Stanislau <it.senkevich@gmail.com>
2024-03-23 16:51:54 +0300

    add order db funcs

--- src/order.cpp
+++ src/order.cpp
@@ -1,3 +1,56 @@
 //
 // Created by hakeyn on 21.3.24.
 //
+
+#include "../include/order.h"
+#include "../include/exceptions.h"
+#include <iostream>
+
+void Order::getDataFromDb(sqlite3 *db, int order_id) {
+    char* sql = "SELECT (driver_id, car_id, date, mileage, load, cost, is_approved) FROM autopark_order WHERE id = ?;";
+    sqlite3_stmt *stmt;
+    int rc = sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr);
+    if (rc != SQLITE_OK) {
+        throw InternalErrorException("Failed to prepare select order statement\n");
+    }
+
+    sqlite3_bind_int(stmt, 1, order_id);
+
+    rc = sqlite3_step(stmt);
+    if (rc != SQLITE_ROW) {
+        throw InternalErrorException("Failed to execute select order statement\n");
+    }
+
+    driver_id = sqlite3_column_int(stmt, 1);
+    car_id = sqlite3_column_int(stmt, 2);
+    date = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 3));
+    mileage = sqlite3_column_double(stmt, 4);
+    load = sqlite3_column_double(stmt, 5);
+    cost = sqlite3_column_double(stmt, 6);
+    is_approved = sqlite3_column_int(stmt, 7);
+}
+
+void Order::insertOrderToDb(sqlite3 *db) {
+    char* sql = "INSERT INTO autopark_order (driver_id, car_id, date, mileage, load, cost, is_approved) "
+                "VALUES (?, ?, ?, ?, ?, ?, ?);";
+
+    sqlite3_stmt *stmt;
+    int rc = sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr);
+    if (rc != SQLITE_OK) {
+        throw InternalErrorException("Failed to prepare insert order statement\n");
+    }
+
+    sqlite3_bind_int(stmt, 1, driver_id);
+    sqlite3_bind_int(stmt, 2, car_id);
+    sqlite3_bind_text(stmt, 3, date.c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_double(stmt, 4, mileage);
+    sqlite3_bind_double(stmt, 5, load);
+    sqlite3_bind_double(stmt, 6, cost);
+    sqlite3_bind_int(stmt, 7, is_approved);
+
+    rc = sqlite3_step(stmt);
+    if (rc != SQLITE_DONE) {
+        throw InternalErrorException("Failed to execute insert user statement\n");
+    }
+    sqlite3_finalize(stmt);
+}
