4323209dea835bff88fa82410d81be1c74c6fd39
Stanislau <it.senkevich@gmail.com>
2024-03-23 14:24:02 +0300

    add validations for driver and and age

--- include/validator.h
+++ include/validator.h
@@ -8,12 +8,24 @@
 #include "config.h"
 #include "order.h"
 #include "driver.h"
+#include <regex>
+#include <ctime>
+#include <chrono>
+#include <sstream>
+#include "exceptions.h"
+
 
 class Validator {
+private:
+    static const std::regex name_pattern;
+    static const std::regex address_pattern;
+    static const std::regex city_pattern;
+    static const std::regex date_pattern;
 public:
     static bool validOrder(const Order& order);
     static bool validDriver(const Driver& driver);
     static bool validLicense(std::string license);
+    static bool validAge(std::string date);
 };
 
 #endif //LAB_5_PROJECT_VALIDATOR_H
--- src/validator.cpp
+++ src/validator.cpp
@@ -3,10 +3,67 @@
 //
 
 #include "../include/validator.h"
-#include <regex>
+#include <iostream>
 
+const std::regex Validator::name_pattern(R"(^[A-Za-zА-Яа-я]+$)");
+const std::regex Validator::address_pattern(R"(^[a-zA-Zа-яА-Я0-9\s.,/]+$)");
+const std::regex Validator::city_pattern(R"(^[A-Za-z]+$)");
+const std::regex Validator::date_pattern(R"(^\d{4}-\d{2}-\d{2}$)");
 
 bool Validator::validLicense(std::string license) {
-    std::regex pattern("^[0-9]{4}[ABEIKMHOPCTX]{2}-[1-7]$");
-    return std::regex_match(license, pattern);
+    std::regex license_pattern("^[0-9]{4}[ABEIKMHOPCTX]{2}-[1-7]$");
+    return std::regex_match(license, license_pattern);
+}
+
+bool Validator::validAge(std::string date_str) {
+    std::tm date = {};
+    std::istringstream ss(date_str);
+    ss >> date.tm_year;  // Year
+    ss.ignore();  // Ignore the dash '-'
+    ss >> date.tm_mon;   // Month
+    ss.ignore();  // Ignore the dash '-'
+    ss >> date.tm_mday; // Day
+
+    auto now = std::chrono::system_clock::now();
+    auto currentTime = std::chrono::system_clock::to_time_t(now);
+    std::tm current = *std::localtime(&currentTime);
+
+    current.tm_year -= 18 - 1900;
+    return std::mktime(&date) <= std::mktime(&current);
+}
+
+bool Validator::validDriver(const Driver &driver) {
+    if (driver.getLogin().empty()) {
+        throw std::invalid_argument("Login must not be empty\n");
+    }
+    if (driver.getPassHash().empty()) {
+        throw std::invalid_argument("Password must not be empty\n");
+    }
+    if (!std::regex_match(driver.getName(), name_pattern)) {
+        throw std::invalid_argument("Name must consist only of russian or latin letters\n");
+    }
+    if (!std::regex_match(driver.getName(), name_pattern)) {
+        throw std::invalid_argument("Surname must consist only of russian or latin letters\n");
+    }
+    if (driver.getExperience() < 0) {
+        throw std::invalid_argument("Experience cannot be negative\n");
+    }
+
+    if (!std::regex_match(driver.getAddress(), address_pattern)) {
+        throw std::invalid_argument("Address must consist only of russian or latin letters\n");
+    }
+
+    if (!std::regex_match(driver.getCity(), city_pattern)) {
+        throw std::invalid_argument("City must consist only of latin letters\n");
+    }
+
+    if (!std::regex_match(driver.getBirthday(), date_pattern)) {
+        throw std::invalid_argument("Birthday must satisfy format YYYY-MM-DD\n");
+    }
+
+    if (!validAge(driver.getBirthday())) {
+        throw std::invalid_argument("Age must be >= 18 years\n");
+    }
+
+    return true;
 }
