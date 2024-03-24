a157bafad5ae483af1228d3bf718ea9515a61fa1
Stanislau <it.senkevich@gmail.com>
2024-03-24 18:38:30 +0300

    rewrite get methods

--- include/controller.h
+++ include/controller.h
@@ -24,12 +24,18 @@ public:
     bool login(const std::string& login, const std::string& password);
     void logout();
 
-    [[nodiscard]] Driver getDriverInfo(int driver_id) const;
-    [[nodiscard]] Order getOrderInfo(int order_id) const;
-    [[nodiscard]] Car getCarInfo(int car_id) const;
     [[nodiscard]] std::vector <Order>& getDriverOrders(int driver_id, std::string period) const;
-    [[nodiscard]] int getWorstDriverId (std::string period) const;
-    [[nodiscard]] int getHighestMileageCarId() const;
+    [[nodiscard]] std::string getCarSummaryMileageAndLoad(int car_id) const;
+    [[nodiscard]] std::string getDriverStatistics(int driver_id) const;
+    [[nodiscard]] std::vector<std::string> getAllDriversStatistics() const;
+    [[nodiscard]] std::string getWorstDriverSummary() const;
+    [[nodiscard]] std::string getBestCarInfo() const;
+
+//    [[nodiscard]] Driver getDriverInfo(int driver_id) const;
+//    [[nodiscard]] Order getOrderInfo(int order_id) const;
+//    [[nodiscard]] Car getCarInfo(int car_id) const;
+//    [[nodiscard]] int getWorstDriverId (std::string period) const;
+//    [[nodiscard]] int getHighestMileageCarId() const;
 
     void addCar(Car& car);
     void addDriver(Driver& driver);
