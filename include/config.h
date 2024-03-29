0b662eff03132f3dd91074a4e998d82eb309cfec
Stanislau <it.senkevich@gmail.com>
2024-03-29 16:50:52 +0300

    implement Config()

--- include/config.h
+++ include/config.h
@@ -6,12 +6,16 @@
 #define LAB_5_PROJECT_CONFIG_H
 
 #include <string>
+#include <sqlite3.h>
+#include "sql.h"
 
 class Config {
 private:
     double driverPercent = 20.;
 
 public:
+    Config() = default;
+    Config(sqlite3* db, const std::string& config_name);
     [[nodiscard]] double getDriverMul() const {
         return driverPercent / 100.;
     }
